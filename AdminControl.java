b964b8296ced8e421f7e116687f5c2faa00e3c5c
KyleTownsend74 <townsend.kyle.kt@gmail.com>
2022-11-01 10:51:29 -0700

    Initial commit

---
+++ AdminControl.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class AdminControl extends JFrame {
+    
+    public AdminControl () {
+        this.setTitle("Twitter");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500, 500);
+        this.setVisible(true);
+    }
+
+}
